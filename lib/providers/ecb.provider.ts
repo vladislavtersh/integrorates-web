6466a998645b39594606b51f08baa499eb92d5f5
vladislavtersh <t@integro.tech>
2026-01-28 18:49:15 +0000

    fix: remove generic from ECBRatesProvider

--- lib/providers/ecb.provider.ts
+++ lib/providers/ecb.provider.ts
@@ -1,33 +1,25 @@
 import { RatesProvider } from "./provider";
 import { ExchangeRate } from "@/lib/rates";
 
-export class ECBRatesProvider implements RatesProvider<ExchangeRate> {
+export class ECBRatesProvider implements RatesProvider {
   async getRates(): Promise<ExchangeRate[]> {
     const res = await fetch(
       "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
       {
-        // серверный компонент → контроль кэша здесь
         cache: "no-store",
       }
     );
-
     if (!res.ok) {
       throw new Error("ECB fetch failed");
     }
-
     const xml = await res.text();
-
-    // дата публикации ECB
     const dateMatch = xml.match(/time=['"]([^'"]+)['"]/);
     const date = dateMatch?.[1] ?? "unknown";
-
-    // курсы валют
     const rateMatches = [
       ...xml.matchAll(
         /currency=['"]([A-Z]{3})['"]\s+rate=['"]([\d.]+)['"]/g
       ),
     ];
-
     return rateMatches.map((m) => ({
       currency: m[1],
       rate: Number(m[2]),
